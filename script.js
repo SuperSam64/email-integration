64628ed8f2a05dd5626c1cc6303decc2d30abbee
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-04-19 20:20:02 -0400

    v1.31

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function body1Click(){
-    $("#body1").text(currentConversation.id + " | " + currentConversation.messages_count + " | " + currentMessage.id)
+    $("#body1").text(currentConversation.id + " | " + currentConversation.messages_count)// + " | " + currentMessage.id)
 }
 function body1Reset(){
     $("#body1").text("[ready]")
