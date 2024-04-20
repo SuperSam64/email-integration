b7f72bd3bef5ad5ec7c60bf19817b3d33b46e453
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-04-19 21:27:04 -0400

    v1.35

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function body1Click(){
-    $("#body1").text(currentConversation.id + " | " + currentConversation.messages_count)// + " | " + currentMessage.id)
+    $("#body1").text(currentConversation.id)
+}
+function body1Click(){
+    $("#body2").text(currentConversation.messages_count)
+}
+function body1Click(){
+    $("#body3").text(currentConversation.latest_message.to_fields[0].address)
+}
+function body1Click(){
+    $("#body4").text(currentConversation.latest_message.from_field.address)
 }
 function body1Reset(){
     $("#body1").text("[ready]")
+}
+function body1Reset(){
+    $("#body2").text("[ready]")
+}
+function body1Reset(){
+    $("#body3").text("[ready]")
+}
+function body1Reset(){
+    $("#body4").text("[ready]")
 }
