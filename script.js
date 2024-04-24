baf170a6db72ca7f6a2dd93ccc23bbfb6f9de6b0
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-04-24 08:33:20 -0400

    v3.32

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ function getFullMessage(conversation){
         var temp = document.createElement('div');
         temp.innerHTML = html;
         var myText = temp.innerText;
-        $("#body16").text(conversation.latest_message.body); // element can be changed
+        $("#body16").text(myText); // element can be changed
         //bodyPlaintext = conversation.latest_message.body.text(); // element can be changed
         //$("#body16").text(bodyPlaintext);
     }
