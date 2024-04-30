74c02d1a1b68aff359ece52d55cc650b3f257bdd
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-04-30 08:53:23 -0400

    v5.81

--- script.js
+++ script.js
@@ -481,6 +481,7 @@ async function showForm(){
                 name: "trackingNumbers",
                 placeholder: 'Enter tracking numbers, 1 per line.\nIf USPS, add ", USPS" after number\nLeave blank if no tracking info is available.\n\nExamples:\n01234567891011121314',
                 value: "",
+                rows: 8
             }
         },{
             type: "select",
@@ -505,7 +506,7 @@ async function showForm(){
         trackingResult = formData.trackingNumbers;
     }
     if(cancelResult == 2) {
-        var returnResult = [
+        returnResult = [
             " If you wish to setup a return you may do so by logging into your filtersfast.com account.",
             " I have initiated a return, and you will be emailed a prepaid return lable which can be used to send your order back for a refund.",
             " I have created a refund-only return, which means your order does not need to be sent back, but we will refund it for you. Please allow 3-7 days for your refund to apply to the original method of payment, and feel free to donate or discard the item(s).",
