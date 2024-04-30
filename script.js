bdd58c5bb65f2db090503554c9a3329cd53de9be
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-04-30 00:14:42 -0400

    v5.71

--- script.js
+++ script.js
@@ -491,11 +491,19 @@ async function showForm(){
             }
         }]
     })
-    var cancelResult = formData.orderCancelled;
+    var cancelResult = [
+        "Your order has been cancelled. Please allow 3-7 days for your refund to apply.",
+        "We apologize as this order has already shipped from our warehouse and can no longer be cancelled"
+    ];
     var trackingResult = formData.trackingNumbers;
-    var returnResult = formData.return;
-    var subscriptionResult = formData.subscriptionCancelled;
-    var fullString = cancelResult + "  " + trackingResult + "  " + returnResult + "  " + subscriptionResult
+    var returnResult = [
+        "If you wish to setup a return you may do so by logging into your filtersfast.com account.",
+        "I have initiated a return, and you will be emailed a prepaid return lable which can be used to send your order back for a refund.",
+        "I have created a refund-only return, which means your order does not need to be sent back, but we will refund it for you. Please allow 3-7 days for your refund to apply to the original method of payment, and feel free to donate or discard the item(s)",
+        "Our custom air filters are non-returnable, however, if a new order is placed, we can offer a full refund for the original order. If you do not wish to place a new order at this time, we can still offer a refund of 50%."
+    ]
+    var subscriptionResult = ["","Thank you for reaching out to us! I have also cancelled your Home Filter Club subscription, and you will receive an email regarding the cancellation."]
+    var fullString = [cancelResult,trackingResult,returnResult,subscriptionResult];
     $("#body1").text(fullString)
     //return Missive.openForm(formData); // this does not work, replace with something that will pass the object to a variable
 }
