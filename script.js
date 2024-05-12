38d5d29ccb2a23aa458a2050a352bda7dc8c59f9
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-05-11 20:02:25 -0400

    v10.23

--- script.js
+++ script.js
@@ -189,19 +189,17 @@ function updateFrom(conversation){
 }
 async function getOrganization(){
     var done = false;
-    var something;
     Missive.fetchLabels().then((labels) => {
         $(labels).each(function(){
           if(this.id.length == 36 && done == false){
-              something = this.organization_id;
-              token = getKey(something);
-              contactBook = getContactsKey(something);
+              organization = this.organization_id;
+              token = getKey(organization);
+              contactBook = getContactsKey(organization);
               console.log(token + " " + contactBook);
               done = true;
             }
         });
     });
-    organization = something;
 }
 function getConversationLink(conversation){
     return "https://mail.missiveapp.com/#inbox/conversations/" + conversation.id;
