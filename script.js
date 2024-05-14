2d5ebe76b68853084ec3b3cb84f12b0bad8e5a7e
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-05-14 17:15:28 -0400

    v10.78

--- script.js
+++ script.js
@@ -936,7 +936,7 @@ async function lookupContact(input){
 		email: input
 	};
     console.log("Contact record: " + contactRecord )
-    if(typeof contactRecord != 'undefined'){
+    if(!!contactRecord){
         contact.firstName = contactRecord.contacts[0].first_name;
         contact.lastName = contactRecord.contacts[0].last_name;  
         for ( var i = 0; i < contactRecord.contacts[0].infos.length; i++ ) {
@@ -976,8 +976,7 @@ async function lookupContact(input){
 
 // ======== BUTTONS ========
 function button1Clicked() {
-    lookupContact(messageFrom);
-    //cancellationReply();
+    cancellationReply();
 }
 function button2Clicked() {
     cancellationNew(); 
