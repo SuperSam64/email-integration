e9b8850b956d4a7daa373de7dcb228d4f83dda45
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-02 14:03:16 -0400

    v11.18.20

--- test/script.js
+++ test/script.js
@@ -174,7 +174,7 @@ async function startup(){
     console.log(currentUser.first_name); // delete later
     await loadData();   
     initialized = true;
-    let color = window.getComputedStyle(element, null).getPropertyValue("background-color");
+    let color = window.getComputedStyle(document.body, null).getPropertyValue("background-color");
     if(1 == 1){
         theme = "dark";
     }
