0726ac4201e769fae0444399cb76154d3fa478dd
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-08 18:25:54 -0400

    v11.72.97

--- test/contact-display.js
+++ test/contact-display.js
@@ -9,7 +9,19 @@ function copyToClipboard(type, duration) {
 		var orderPosition = (type.slice(5,6) * 1);
 		var clipboard = document.getElementById("orderField" + orderPosition).innerText;
 		clipboard = clipboard.replace("Order #","");
-		//navigator.clipboard.writeText(clipboard);
+		const textArea = document.createElement("textarea");
+        textArea.value = clipboard;
+        //document.body.appendChild(textArea);
+        document.getElementById("contactInfoSection").appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+            document.execCommand('copy');
+        } catch (err) {
+            console.error('Unable to copy to clipboard', err);
+        }
+        document.getElementById("contactInfoSection").removeChild(textArea);
+        //document.body;
 		var popup = document.getElementById("orderPopup" + orderPosition);
 		popup.innerText = "Order #" + clipboard + " copied!";
 	}
@@ -45,11 +57,10 @@ function copyToClipboard(type, duration) {
 		else{
 			clipboard = remove[index];
 		}
-
-
         const textArea = document.createElement("textarea");
         textArea.value = clipboard;
-        document.body.appendChild(textArea);
+        //document.body.appendChild(textArea);
+        document.getElementById("contactInfoSection").appendChild(textArea);
         textArea.focus();
         textArea.select();
         try {
@@ -57,18 +68,8 @@ function copyToClipboard(type, duration) {
         } catch (err) {
             console.error('Unable to copy to clipboard', err);
         }
-        document.body.removeChild(textArea);
-
-        navigator.permissions.query({
-            name: "clipboard-write"
-        }).then((result) => {
-            if (result.state === "granted" || result.state === "prompt"){
-                navigator.clipboard.writeText(clipboard);
-            }
-            else{
-                console.log("no permission")
-            }
-        })
+        document.getElementById("contactInfoSection").removeChild(textArea);
+        //document.body;
 		var popup = document.getElementById(type + "Popup");
 		popup.innerText = messages[index];
 	}
@@ -88,13 +89,13 @@ function buildOrderNumbersList(list){
 		var orderArray = [];
 		for(var i = 0; i < list.length; i++){
 			orderArray.push(
-				'<span class="fieldText" id="orderField' + i +
+				'<span class="fieldText" style="margin-top:6px" id="orderField' + i +
 				'" onclick="copyToClipboard(' + "'order" + i + "'" + ',' + animationLength + ')">' +
 				'Order #' + orderNumbersList[i] + '<span class="popup" id="orderPopup' + i +
 				'"></span></span>'
 			);
 		}
-		document.getElementById("orderNumberList").innerHTML = orderArray.join("<br>");
+		document.getElementById("orderNumberList").innerHTML = orderArray.join("<br>").replace('style="margin-top:6px" ','');
 	}
 }
 function trigger(){
