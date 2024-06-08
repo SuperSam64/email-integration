dfab0138b6767616b7a505e355cd6e3b6e647a14
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-08 18:46:29 -0400

    v11.72.98

--- test/script.js
+++ test/script.js
@@ -1132,7 +1132,9 @@ function contactFormSave(first,second,third){
     // blank, leading spaces, mix of capitalization
     var formEmail = document.getElementById('formEmail').value.trim().replaceAll(" ","").toLowerCase();
     // keep CP09 but remove CP, - # . all spaces
-    var formOrderNumbers = ("|," + document.getElementById('formOrderNumbers').value).trim().split(",").shift();
+    var formOrderNumbers = "Order #" + document.getElementById('formOrderNumbers').value.trim().replaceAll("CP09","[prefix]")
+        .replaceAll("Cp","").replaceAll("cP","").replaceAll("cp","").replaceAll("CP","").replaceAll("-","")
+        .replaceAll("#","").replaceAll(" ","").replaceAll("[prefix]","CP09-").replaceAll(",,",",").replaceAll(",","Order #").split(",").shift();
     //for loops to normalize order numbers, emails, customer IDs, names, and order numbers. consider empty values.
     console.log("saved, " + formFirstName  + ", " + formLastName  + ", " + formFullname  + ", " + formCustID  + ", " + formPhoneNumber  + ", " + formEmail  + ", " + formOrderNumbers);
 }
