ec3d8936f00985c6df48e181ea821c57766e6171
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-12 19:06:54 -0400

    v11.80.242

--- test/script.js
+++ test/script.js
@@ -1083,29 +1083,29 @@ async function lookupContact(input){
     if(typeof contactRecord.contacts[0] != 'undefined'){
         contact.firstName = normalizeFirstName(contactRecord.contacts[0].first_name,"edit");
         contact.lastName = normalizeFirstName(contactRecord.contacts[0].last_name,"edit");
-        contact.fullName = normalizeFullName(contact.firstName,contact.lastName,"edit",false);
+        contact.fullName = normalizeFullName(contact.firstName,contact.lastName,"input",true);
         for ( var i = 0; i < contactRecord.contacts[0].infos.length; i++ ) {
             if(typeof contactRecord.contacts[0].infos[i].kind != 'undefined') {
                 if(contactRecord.contacts[0].infos[i].kind == "phone_number" && contactRecord.contacts[0].infos[i].value != "" && contactRecord.contacts[0].infos[i].value != "Phone number"){
-                    contact.phoneNumber = normalizePhoneNumber(contactRecord.contacts[0].infos[i].value,"edit",false);
+                    contact.phoneNumber = normalizePhoneNumber(contactRecord.contacts[0].infos[i].value,"input",true);
                 }
             }
         }
         for ( var i = 0; i < contactRecord.contacts[0].infos.length; i++ ) {
             if(typeof contactRecord.contacts[0].infos[i].custom_label != 'undefined') {
                 if(contactRecord.contacts[0].infos[i].custom_label.toLowerCase() == "customer id" && contactRecord.contacts[0].infos[i].value != "" && contactRecord.contacts[0].infos[i].value != "Customer ID"){
-                    contact.customerID = normalizeCID(contactRecord.contacts[0].infos[i].value,"edit",false);
+                    contact.customerID = normalizeCID(contactRecord.contacts[0].infos[i].value,"input",true);
                 }
             }
         }
         contactExists =  true;
     }
-    contact.email = normalizeEmail(input,"edit",false);
+    contact.email = normalizeEmail(input,"input",true);
     console.log("First name " + contact.firstName);
     console.log("Last name " +  contact.lastName);
-    console.log("CID " + customerID);
-    console.log("Phone number" + phoneNumber);
-    console.log("Email " + email);
+    console.log("CID " + contact.customerID);
+    console.log("Phone number" + contact.phoneNumber);
+    console.log("Email " + contact.email);
 
     /*if(
         conversationSubject.slice(0,6) == "Orders" &&
