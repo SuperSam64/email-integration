b6a43937f231ae3a4fb2c90f129240f37f4bf0ab
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-13 01:59:20 -0400

    v11.80.265

--- test/script.js
+++ test/script.js
@@ -1393,7 +1393,7 @@ function normalizeFullName(first,last,type,updateElements){
 }
 function normalizeCID(input,type,updateElements){
     var output;
-    var raw = input.replace("Customer ID").replace("CID").trim().replaceAll(" ","").toUpperCase();
+    var raw = input.replace("Customer ID","").replace("CID","").trim().replaceAll(" ","").toUpperCase();
     if(raw.slice(0,3) != "CUS"){
         raw = raw * 1;
     }
