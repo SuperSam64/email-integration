4a19a7ba9e39344fbdc53644ff7553d37b5fbaa1
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2024-06-17 22:42:22 -0400

    v11.81.463

--- test/contact-display.js
+++ test/contact-display.js
@@ -50,7 +50,7 @@ async function lookupContact(input){
         contactExists =  true;
     }
     else if(customerName != "" && customerName != 'undefined' && typeof customerName != 'undefined'){
-        contact.firstName = normalizeFirstName(contactRecord.contacts[0].first_name,"panel");
+        contact.firstName = normalizeFirstName(customerName,"panel");
     }
     contact.email = normalizeEmail(input,"panel",true);
     contactFormSave(contact.fullName,contact.customerID,contact.phoneNumber,contact.email,contact.email);
--- test/script.js
+++ test/script.js
@@ -1,6 +1,6 @@
 baseVersion = "11";
 revisionNumber = "81";
-scriptVersion = "462";
+scriptVersion = "463";
 
 /*
 CID - should remove "CID " when going from the text to the form
