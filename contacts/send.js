e0da71b5d288852364b8d6d75635e510cacdf6e5
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-03-20 01:21:29 -0400

    Update send.js

--- contacts/send.js
+++ contacts/send.js
@@ -1,34 +1,27 @@
-// send.js:31
-function makeContact(body) {
-  // Use proxy URL instead of original URL
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  var key=prompt('Key');
-  var cust=prompt('Customer');
-  var targetUrl='https://application.textline.com/api/customer/'+cust+'.json?access_token='+key;
-  var body = {
-    'customer': {
-      'phone_number': '(222) 222-2222',
-      'email': 'chuck@mycompany.com',
-      'name': 'Chuck Finley',
-      'notes': 'some samples notes for the contact',
-      'tags': 'foo_bar'
-    }
-  };
-  const request = new XMLHttpRequest();
-  request.open('POST', proxyUrl + targetUrl, true); // Add proxyUrl to front of the targetUrl
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
-      console.log("Request successful:", this.responseText);
-    } else {
-      // Error handling
-      console.error("Request failed with status:", this.status);
-    }
-  };
-  request.onerror = function() {
-    // Connection error handling
-    console.error("Connection error");
-  };
-  request.send(JSON.stringify(body));
+async function makeContact(e){	
+	var cust=prompt('Customer');
+	var key=prompt('Key');
+	var url='https://application.textline.com/api/customer/'+cust+'.json
+
+	
+	const response = await fetch ("https://api.monday.com/v2", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-TGP-ACCESS-TOKEN' : key
+		},
+		body: JSON.stringify({
+			'customer': {
+				'phone_number': '(222) 222-2222',
+				'email': 'chuck@mycompany.com',
+				'name': 'Chuck Finley',
+				'notes': 'some samples notes for the contact',
+				'tags': 'foo_bar'
+			}
+		})
+	})
+
+	.catch(error => {
+		console.error('Error:', error);
+	}
 }
