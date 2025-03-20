7d296ca4c17adcd39f5eb26a3e580a4470d612ad
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-03-20 01:03:10 -0400

    Update send.js

--- contacts/send.js
+++ contacts/send.js
@@ -1,32 +1,34 @@
-
-
-function makeContact(){
-	var key=prompt('Key');
-	var cust=prompt('Customer');
-	var url='https://application.textline.com/api/customer/'+cust+'.json?access_token='+key;
-	var request = new XMLHttpRequest();
-
-	request.open('POST', url);
-
-	request.setRequestHeader('Content-Type', 'application/json');
-
-	request.onreadystatechange = function () {
-	  if (this.readyState === 4) {
-		console.log('Status:', this.status);
-		console.log('Headers:', this.getAllResponseHeaders());
-		console.log('Body:', this.responseText);
-	  }
-	};
-
-	var body = {
-	  'customer': {
-		'phone_number': '(222) 222-2222',
-		'email': 'chuck@mycompany.com',
-		'name': 'Chuck Finley',
-		'notes': 'some samples notes for the contact',
-		'tags': 'foo_bar'
-	  }
-	};
-
-	request.send(JSON.stringify(body));
+// send.js:31
+function makeContact(body) {
+  // Use proxy URL instead of original URL
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  var key=prompt('Key');
+  var cust=prompt('Customer');
+  var targetUrl='https://application.textline.com/api/customer/'+cust+'.json?access_token='+key;
+  var body = {
+    'customer': {
+      'phone_number': '(222) 222-2222',
+      'email': 'chuck@mycompany.com',
+      'name': 'Chuck Finley',
+      'notes': 'some samples notes for the contact',
+      'tags': 'foo_bar'
+    }
+  };
+  const request = new XMLHttpRequest();
+  request.open('POST', proxyUrl + targetUrl, true); // Add proxyUrl to front of the targetUrl
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      console.log("Request successful:", this.responseText);
+    } else {
+      // Error handling
+      console.error("Request failed with status:", this.status);
+    }
+  };
+  request.onerror = function() {
+    // Connection error handling
+    console.error("Connection error");
+  };
+  request.send(JSON.stringify(body));
 }
