406de91ad825c16ab2c63e67df4c7222933fd545
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-06-02 10:07:36 -0400

    Update add_contact.js

--- textline/add_contact.js
+++ textline/add_contact.js
@@ -4,10 +4,10 @@ function getParams(){
   var queryString=window.location.search;
   var urlParams=new URLSearchParams(queryString);
   return {
-    name:URLSearchParams.get('name'),
-    phone:URLSearchParams.get('phone'),
-    email:URLSearchParams.get('email'),
-    CID:URLSearchParams.get('CID'),
-    order:URLSearchParams.get('order')
+    name:urlParams.get('name'),
+    phone:urlParams.get('phone'),
+    email:urlParams.get('email'),
+    CID:urlParams.get('CID'),
+    order:urlParams.get('order')
   }
 }
