c08e6483baad9534c2e4dd76b837f637087bc8be
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-06-07 20:43:53 -0400

    Update add_contact.js

--- textline/add_contact.js
+++ textline/add_contact.js
@@ -3,8 +3,9 @@ console.log(contact);
 function getParams(){
   var urlParams=new URLSearchParams(window.location.search);
   return {
+    key:urlParams.get('key'),
+    uuid:urlParams.get('uuid'),
     name:urlParams.get('name'),
-    phone:urlParams.get('phone'),
     email:urlParams.get('email'),
     CID:urlParams.get('CID'),
     order:urlParams.get('order')
