3141705ee49e220ce4edc962d50e7eac35eb8e07
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-06-15 14:36:30 -0400

    Update add_contact.js

--- textline/add_contact.js
+++ textline/add_contact.js
@@ -1,34 +1,45 @@
-var params=getParams();
-var key=params[0];
-var contact=params[1];
+storeContactData(getParams()[0],getParams()[1],getParams()[2]);
+
 function getParams(){
   var urlParams=new URLSearchParams(window.location.search);
-  return [urlParams.get('key'),{
-    uuid:urlParams.get('uuid'),
-    name:urlParams.get('name'),
-    email:urlParams.get('email'),
-    CID:urlParams.get('CID'),
-    order:urlParams.get('order')
+  return [
+    urlParams.get('key'),
+    uuid:urlParams.get('uuid'), 
+    {     
+      phone_number:urlParams.get('phone_number'),
+      name:urlParams.get('name'),
+      email:urlParams.get('email'),
+      order:urlParams.get('order')
   }];
 }
-function createContact(key,data){
-  var request = new XMLHttpRequest();
-  request.open('POST', 'https://private-anon-a941205863-textline.apiary-proxy.com/api/customers.json?access_token='+key);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      console.log('Status:', this.status);
-      console.log('Headers:', this.getAllResponseHeaders());
-      console.log('Body:', this.responseText);
-    }
-  };
-  var body = {
-    'customer': {
-      'name':data.name,
-      'email': data.email,
-      'notes': data.CID,
-      'tags': data.order
-    }
-  };
-  request.send(JSON.stringify(body));
+
+function storeContactData(contact,key,uuid){
+	var request = new XMLHttpRequest();
+	if(typeof uuid==='undefined'){
+		request.open('POST', 'https://private-anon-a941205863-textline.apiary-proxy.com/api/customers.json?access_token='+key);
+	}
+	else{
+		request.open('PUT', 'https://private-anon-c310406e46-textline.apiary-proxy.com/api/customer/'+uuid+'.json?access_token='+key);
+	}
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.onreadystatechange = function () {
+		if (this.readyState === 4) {
+		console.log('Status:', this.status);
+		console.log('Headers:', this.getAllResponseHeaders());
+		console.log('Body:', this.responseText);
+		}
+	};
+	var body = {
+		'customer': {
+			'email': contact.email,
+			'name': contact.name,
+			'tags': contact.order,
+			'tags_handling': 'add',
+			'custom_fields':{
+				'f3310f75-3371-4620-a41a-bacad6a73ee0':contact.CID
+			}
+		}
+	};
+	if(typeof uuid==='undefined'){body.customer.phone_number=contact.phone_number};
+	request.send(JSON.stringify(body));
 }
