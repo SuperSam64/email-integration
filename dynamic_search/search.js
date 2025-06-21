27ca70d4588cc12198efb69b4577d66f556e565f
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-06-21 15:28:50 -0400

    Update search.js

--- dynamic_search/search.js
+++ dynamic_search/search.js
@@ -1,13 +1,11 @@
 function getParams(){
 	var urlParams=new URLSearchParams(window.location.search);
 	var query=urlParams.get(query);
-  switch(query){
-    case 'something';
-      alert('something');
-      break;
-    default:
-      alert('not something');
-      break;
-  }
-  return
+	if(query=='something'){
+		window.location.href='https://www.google.com/search?q='+query;
+	}
+	else{
+		window.location.href='https://www.filtersfast.com/search/?query='+query;
+	}	
+  return;
 }
