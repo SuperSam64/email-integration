0065042da2ad67e0365934a252184fe2b2a023b1
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-08-23 11:05:07 -0400

    Update search.js

--- monday_global_search/search.js
+++ monday_global_search/search.js
@@ -1,6 +1,9 @@
 console.log('hello world!');
+var URL = window.location.search;
+var parameters = new URLSearchParams(URL);
+console.log(parameters.get('key');
 /*console.log(globalSearch('12345678','abcd'));*/
-async function getMondayData(searchTerm,key){
+async function getMondayData(term,key){
     const response = await fetch ("https://api.monday.com/v2", {
         method: 'post',
         headers: {
