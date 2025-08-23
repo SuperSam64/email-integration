933870f46e63907688f72f2d5c1ebbce824f5e48
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-08-23 11:59:38 -0400

    Update search.js

--- monday_global_search/search.js
+++ monday_global_search/search.js
@@ -5,7 +5,7 @@ var query = parameters.get('query');
 var key = parameters.get('key');
 getResults();
 fucntion getResults(){
-    window.open("javascript:window.location.href='filtersfast.monday.com/search/cross_board?q=' + query; console.log('this is a test')");
+    window.open("javascript:window.location.href='https://filtersfast.monday.com/search/cross_board?q=' + query; console.log('this is a test')");
 }
 /*async function globalSearch(term,token){
     const response = await fetch ("https://filtersfast.monday.com/search/cross_board?q" + term, {
