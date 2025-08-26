e3a868071252152e62cac707e1ecb7ecd429be59
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-08-26 15:58:42 -0400

    Update photos.js

--- textline/photos/photos.js
+++ textline/photos/photos.js
@@ -8,7 +8,7 @@ function getParams(){
 
 function getConversationId(key, phone_number){
 	var request = new XMLHttpRequest();
-	request.open('GET', 'https://private-anon-6123db9648-textline.apiary-proxy.com/api/customers.json?phone_number='+phone_number+'&access_token='+key); /* a941205863 */
+	request.open('GET', 'https://private-anon-6123db9648-textline.apiary-proxy.com/api/conversations.json?phone_number='+phone_number+'&access_token='+key); /* a941205863 */
 	request.onreadystatechange = function () {
 		if (this.readyState === 4) {
 			if(JSON.parse(this.response).customer==null){
