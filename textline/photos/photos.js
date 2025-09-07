050b41715b9d435aefa380610fa73eecb9b00156
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-09-06 22:06:57 -0400

    Update photos.js

--- textline/photos/photos.js
+++ textline/photos/photos.js
@@ -158,21 +158,23 @@ function setClickBehavior(){
 		   }
 		});
 		document.addEventListener('keydown', (event) => {
-			switch (event.key) {
-			case 'ArrowLeft':
-				event.stopPropogation;
-				previous();
-				break;
-			case 'ArrowRight':
-				event.stopPropogation;
-				next();
-				break;
-			case 'Escape':
-				event.stopPropogation;
-				close();
-				break;
-			default:
-				break;
+			if(modal.classList.contains('is-visible')){
+				switch (event.key) {
+				case 'ArrowLeft':
+					event.stopPropogation;
+					previous();
+					break;
+				case 'ArrowRight':
+					event.stopPropogation;
+					next();
+					break;
+				case 'Escape':
+					event.stopPropogation;
+					close();
+					break;
+				default:
+					break;
+				}
 			}
 		});
 	});
