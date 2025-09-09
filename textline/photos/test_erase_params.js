8d969308697c9e095cf9fd6c4a26e6c2331dfe39
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-09-09 08:19:30 -0400

    Update test_erase_params.js

--- textline/photos/test_erase_params.js
+++ textline/photos/test_erase_params.js
@@ -1,5 +1,5 @@
 function getParams(){
 	var urlParams=new URLSearchParams(window.location.search);
 	var testval=urlParams.get('testval');
-	window.open('javascript:window.location.href="https://www.google.com";console.log('+testval+', "_self")')
+	window.open('javascript:window.location.href="https://supersam64.github.io/email-integration/textline/photos/test_erase_params.html";console.log('+testval+','_self')
 }
