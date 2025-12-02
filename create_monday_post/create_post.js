21d660726f62e7b051255e5337798ea228e99735
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-02 11:36:01 -0500

    Update create_post.js

--- create_monday_post/create_post.js
+++ create_monday_post/create_post.js
@@ -1,7 +1,33 @@
 var key = getKey();
+
 function getKey(){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const key = urlParams.get('key');
   return key;
+}
+
+async function createPost(){
+    const response = await fetch ("https://api.monday.com/v2", {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : key,
+            'API-Version' : '2025-10'
+        },
+        body: JSON.stringify({
+          'query' : 
+            `mutation {
+              create_item(
+                board_id: 18390618196
+                item_name: "new item"
+                group_id: "topics"
+              ),
+              {
+                id
+              }
+            }`
+        })
+    })
+    return response.json();
 }
