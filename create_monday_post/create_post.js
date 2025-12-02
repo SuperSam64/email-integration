705d81bd0d81ababac7e18290d32c2ece336940b
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-02 11:53:52 -0500

    Update create_post.js

--- create_monday_post/create_post.js
+++ create_monday_post/create_post.js
@@ -1,10 +1,13 @@
 var key = getKey();
-console.log(createPost());
 function getKey(){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const key = urlParams.get('key');
   return key;
+}
+
+function doAthing(input){
+  console.log(input);
 }
 
 async function createPost(){
@@ -29,7 +32,6 @@ async function createPost(){
             }`
         })
     })
-    result = response.json();
-    console.log('*' + result);
-    return result;
+    doAthing(response.json());
+    return;
 }
