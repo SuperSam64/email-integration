efba50b064449fbff022a6690f90b2d6079b404c
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-05 19:46:48 -0500

    Update create_post.js

--- create_monday_post/create_post.js
+++ create_monday_post/create_post.js
@@ -1,14 +1,77 @@
-var key = getKey();
-function getKey(){
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const key = urlParams.get('key');
-  return key;
-}
-function createUpdate(item){
-	console.log(item);
-}
+var itemData = getParams();
+var key = itemData.key;
 createItem();
+function getParams(){
+	var params = new URLSearchParams(window.location.search);
+	var requestType = params.get('requestType');
+  	var mentions = params.get('mentions') ? params.get('mentions').split(',') : [];
+	var mentionsString = '';
+	for(i = 0; i < mentions.length; i++){
+		mentionsString += '{id: ' + mentions[i] + ' type: User}';
+	}
+	var itemObject = {
+		key: params.get('key'),
+		info: {
+			shipper: params.get('shipper'),
+			shipperNum: params.get('shipperNum'),
+			CID: params.get('CID'),
+			orderNum: params.get('orderNum'),
+			refundReason: params.get('refundReason'),
+			refundAmount: params.get('refundAmount'),
+			refundType: params.get('refundType') ? ' (' + params.get('refundType') + ')': ''
+		}
+	};
+	switch(requestType){
+		case 'refund':
+			itemObject.item = {
+				board_id: 169806972,
+				item_name: 'Cancelled order #' + itemObject.info.orderNum + ' / ' + itemObject.info.CID + ' / $' + itemObject.info.refundAmount + itemObject.info.refundType,
+				group_id: 'refund_requests',
+				columns: 'status,' + itemObject.info.refundReason
+			};
+			itemObject.update = {
+				body: params.get('update') ? params.get('update') : '',
+				mentions_list: 'mentions_list: [], '
+			};
+			break;
+		case 'large refund':
+			itemObject.item = {
+				board_id: 169806972,
+				item_name: 'Cancelled order #' + itemObject.info.orderNum + ' / ' + itemObject.info.CID + ' / $' + itemObject.info.refundAmount + itemObject.info.refundType,
+				group_id: 'new_group71812',
+				columns: 'status,' + itemObject.info.refundReason
+			};
+			itemObject.update = {				
+				body: (params.get('update') ? params.get('update') + '<br>' : '') + 'FYI',
+				mentions_list: 'mentions_list: [{id: ' + 35911561/*20525251 */ + ' type: User}], '
+			};
+			break;
+		case 'shipping request':
+			itemObject.item = {
+				board_id: 768866444,
+				item_name: 'B2B order #' + itemObject.info.orderNum,
+				group_id: 'topics'
+			};
+			itemObject.update = {
+				body: 'Please ship using ' + itemObject.info.shipper + ' account #' + itemObject.info.shipperNum + ', thank you!<br>',
+				mentions_list: 'mentions_list: [{id: ' + 35911561/*20525251 */ + ' type: User}{id: ' + 35911561/*20525251 */ + ' type: User}], '
+			};
+			break;
+		default:
+			itemObject.item = {
+				board_id: params.get('board'),
+				item_name: params.get('name'),
+				group_id: params.get('group'),
+				columns: params.get('columns')
+			};
+			itemObject.update = {
+				body: params.get('update') ? params.get('update') : '',
+				mentions_list: 'mentions_list: [' + (mentionsString == '' ? '' : mentionsString) + '], '
+			};
+			break;
+	}
+ 	return itemObject;
+}
 async function createItem(){
     const response = await fetch ("https://api.monday.com/v2", {
         method: 'post',
@@ -21,9 +84,9 @@ async function createItem(){
           'query' : 
             `mutation {
               create_item(
-                board_id: 18390618196
-                item_name: "new item"
-                group_id: "topics"
+                board_id: ` + itemData.item.board_id + `
+                item_name: "` + itemData.item.item_name + `",
+                group_id: "` + itemData.item.group_id + `",
               ),
               {
                 id
@@ -32,5 +95,61 @@ async function createItem(){
         })
     });
     var item = await response.json();
-    createUpdate(item.data.create_item.id);
+	updateColumn(item.data.create_item.id, itemData.item.board_id, itemData.item.columns);
+	createUpdate(item.data.create_item.id, itemData.item.board_id);
+}
+function createUpdate(item, board){
+	if(itemData.update.body && itemData.update.mentions_list){
+		fetch ("https://api.monday.com/v2", {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization' : key,
+				'API-Version' : '2025-10'
+			},
+			body: JSON.stringify({
+			  'query' : 
+				`mutation {
+			create_update (item_id: ` + item + `, ` + itemData.update.mentions_list + ` body: "` + itemData.update.body + `") {
+				id
+			}
+		}`
+			})
+		});
+	}
+	goToItem(item, board);
+}
+function updateColumn(item, board, updatesString){
+	if(updatesString){
+		var columns = updatesString.split(';');
+		var updates = [];
+		for(i = 0; i < columns.length; i ++){
+			if(columns[i] != ''){
+				updates.push(`\\"` + columns[i].split(',')[0] + `\\": {\\"label\\": \\"` + columns[i].split(',')[1] + `\\"}`);
+			}
+		}
+		var updatesOutput = '"{' + updates.join(',') + '}"';
+		fetch ("https://api.monday.com/v2", {
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization' : key,
+					'API-Version' : '2025-10'
+				},
+				body: JSON.stringify({
+				'query': `mutation {
+					change_multiple_column_values (
+						board_id: ` + board + `,
+						item_id: ` + item + `,
+						column_values: ` + updatesOutput + `
+					) {
+						id
+					}
+				}`
+			})
+		});
+	}
+}
+function goToItem(item, board){
+	window.location.href = 'https://filtersfast.monday.com/boards/' + board + '/pulses/' + item;
 }
