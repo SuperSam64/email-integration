62aeaedf52450331a906f14de6d312d1b7b22b02
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-07 18:39:12 -0500

    Update send.js

--- create_monday_post/form_by_email/send.js
+++ create_monday_post/form_by_email/send.js
@@ -1,10 +1,22 @@
-console.log(window.location.href);
-var missive_po_key = localStorage.getItem('missive_po_key');
-console.log('Key:', missive_po_key);
-function openForm(){
+
+/*var missive_po_key = localStorage.getItem('missive_po_key');
+console.log('Key:', missive_po_key);*/
+
+var testKey = localStorage.getItem('missive_po_key');
+console.log('Key:', testKey);
+
+/*function openForm(){
   var URL = window.location.href;
   var conversation = URL.split('conversations/')[1];
   var messageLink = 'https://mail.missiveapp.com/#inbox/conversations/' + conversation;
   var formLink = 'https://forms.monday.com/forms/3ae2e16a1b03593499e614a01d99d276?r=use1&missive=' + messageLink;
   window.open(formLink);
+}*/
+
+function openForm(){
+  var keyField = document.getElementById('keyField');
+  var key = keyField.textContent;
+  localStorage.setItem('missive_po_key', key);
+  var missive_po_key = localStorage.getItem('missive_po_key');
+  console.log('Key:', missive_po_key);
 }
