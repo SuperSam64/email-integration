e40c70c502d266f57b2728de9aec2efb9b0fd02d
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-07 17:53:47 -0500

    Update send.js

--- create_monday_post/form_by_email/send.js
+++ create_monday_post/form_by_email/send.js
@@ -1,1 +1,5 @@
+var URL = window.location.href;
+var conversation = URL.split('conversations/')[1];
+var messageLink = 'https://mail.missiveapp.com/#inbox/conversations/' + conversation;
+
 console.log(window.location.href);
