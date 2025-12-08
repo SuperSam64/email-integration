c435b4c9a5f8311cb9a14c3fb52b16788a69450c
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-07 19:23:35 -0500

    Update send.js

--- create_monday_post/form_by_email/send.js
+++ create_monday_post/form_by_email/send.js
@@ -15,9 +15,29 @@ console.log('Key:', testKey);
 }*/
 
 function openForm(){
+  Missive.fetchConversations(ids).then((conversations) => {
+    if (conversations.length != 1) {
+      // Do nothing if multiple conversations are selected.
+      return
+    }
+
+    var message = conversations[0].latest_message
+    if (!message || !message.from_field) {
+      // Do nothing if conversation has no message (only chat comments) or if
+      // message has no From field.
+      return
+    }
+
+    var from = message.from_field
+    console.log('Message from:', from.name, from.address)
+    console.log('Message subject:', message.subject)
+  })
+}
+
+/*function openForm(){
   var keyField = document.getElementById('keyField');
   var key = keyField.textContent;
   localStorage.setItem('missive_po_key', key);
   var missive_po_key = localStorage.getItem('missive_po_key');
   console.log('Key:', missive_po_key);
-}
+}*/
