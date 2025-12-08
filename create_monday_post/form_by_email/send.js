704ef168d87655d3b4aedeff95a39a29dc19a830
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-12-07 20:13:35 -0500

    Update send.js

--- create_monday_post/form_by_email/send.js
+++ create_monday_post/form_by_email/send.js
@@ -1,7 +1,7 @@
 var testKey = localStorage.getItem('missive_po_key');
 var currentConvo;
 function openForm(){
-  var messageLink = 'https://mail.missiveapp.com/#inbox/conversations/' + currentConvo;
+  var messageLink = 'https://forms.monday.com/forms/3ae2e16a1b03593499e614a01d99d276?r=use1&missive=https://mail.missiveapp.com/#inbox/conversations/' + currentConvo;
   window.location.href=messageLink;
 }
 Missive.on('change:conversations', (ids) => {
