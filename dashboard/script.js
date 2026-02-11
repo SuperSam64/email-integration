03bf33beb5d74b2048d4c94740459a33f50324a3
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-02-11 15:14:47 -0500

    Update script.js

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,1 +1,9 @@
-
+var data = getParams(window.location.href.split('?')[0], 'Dashboard');
+console.log(data);
+function getParams(newURL, newTitle, args){
+	var output = Object.fromEntries(new URLSearchParams(window.location.search));
+	if(newURL){
+		history.replaceState(output, newTitle ? newTitle : '', newURL);
+	}
+	return output;
+}
