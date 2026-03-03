31cc285c96c35ead7f9d191a699eaf15e77a8613
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-03-03 07:10:31 -0500

    Update definitions.js

--- decision_tree/definitions.js
+++ decision_tree/definitions.js
@@ -1,46 +1,130 @@
 var data = {
-  'xMain Menu': {
-    title: 'Main Menu',
-    'xCancellation': {
-      title: 'Cancellation',
-      info: 'Cancel an order',
-      'xCancel outcomes': {
-        title: 'Cancel outcomes',
-        info: 'Cancel an order',
-        'xCannot cancel': {
-          title: 'Cannot cancel',
-          info: 'Order could not be cancelled',
-          'xEmail to advise': {
-            title: 'Email to advise',
-            info: 'Email customer to advise cancellation was not possible',
-          }
-        },
-        'xCan be cancelled': {
-          title: 'Can be cancelled',
-          info: 'Order can be cancelled',
-          'xEmail to advise': {
-            title: 'Email to advise',
-            info: 'Email customer to advise cancellation order was cancelled',
-          }
-        },
-        'xCan be partially cancelled': {
-          title: 'Can be partially cancelled',
-          info: 'Order could not be fully cancelled, but could be partially cancelled',
-          'xEmail to advise': {
-            title: 'Email to advise',
-            info: 'Email customer to advise order was partially cancelled',
-          }
-        }
-        
-      }
-    },
-    'xDamage':{
-      title: 'Damage',
-      info: 'Order was damaged',
-      'xReship':{
-        title: 'Reship',
-        info: 'Reship damaged item(s)'
-      }
-    }
-  }
+	'Main Menu': {
+		title: 'Main Menu',
+		'Return items': {
+			title: 'Customer needs to return items',
+			info: '',
+			'Eligible': {
+				title: 'Item(s) to return are eligible',
+				info: '',
+				'xCannot cancel': {
+					title: 'Cannot cancel',
+					info: 'Order could not be cancelled',
+					'xEmail to advise': {
+						title: 'Email to advise',
+						info: 'Email customer to advise cancellation was not possible',
+					}
+				},
+				'xCan be cancelled': {
+					title: 'Can be cancelled',
+					info: 'Order can be cancelled',
+					'xEmail to advise': {
+						title: 'Email to advise',
+						info: 'Email customer to advise cancellation order was cancelled',
+					}
+				},
+				'xCan be partially cancelled': {
+					title: 'Can be partially cancelled',
+					info: 'Order could not be fully cancelled, but could be partially cancelled',
+					'xEmail to advise': {
+						title: 'Email to advise',
+						info: 'Email customer to advise order was partially cancelled',
+					}
+				}
+				
+			},
+			'Custom': {
+				title: 'Item(s) to return are not eligible',
+				info: 'See "Custom Air Filters" section (link to be added later)'
+			},
+			'Non-returnable': {
+				title: 'Other non-returnable items',
+				info: '',
+				'Refund': {
+					title: 'A refund (or partial refund) is appropriate',
+					info: ''
+				},
+				'No refund': {
+					title: 'A refund (or partial refund) is not appropriate',
+					info: 'Explain to the customer that we will unfortunately not be able to accept the return, as it is ineligible/outside of the return window'
+				}
+			},
+			'Past return window': {
+				title: 'Order was placed more than 365 days ago',
+				info: '',
+				'xCannot cancel': {
+				
+				}				
+			}
+			,
+			'Sent in error': {
+				title: 'Customer received item(s) in error, and has not placed an order',
+				info: '',
+				'Send labels': {
+					title: 'Send return label(s)',
+					info:'Send as many labels as needed using the “Generate Return Label [LINK]” link. When you create labels, choose the “Save as PDF” option. Make a [Monday request] to let CRMs know about the item(s) being returned, and get as much info as possible to help us identify order/customer details.'
+				}				
+			}
+		}/*,
+		'Overshipped/short shipped': {
+			
+		},
+		'Tax exemption': {
+			
+		},
+		'Issues with custom air filters':{
+			
+		},
+		'Damaged | Defective': {
+			
+		},
+		'Order processing issues': {
+			
+		},
+		'Address update needed': {
+			
+		},
+		'Webdev': {
+			
+		},
+		'Product Info': {
+			
+		},
+		'Order Status': {
+			
+		},
+		'Cancellations': {
+			
+		},
+		'B2B call': {
+			
+		},
+		'Account issues': {
+			
+		},
+		'Data removal': {
+			
+		},
+		'Escalation': {
+			
+		},
+		'Invoice request': {
+			
+		},
+		'Inaccurate tracking information': {
+			
+		},
+		'Incompatible': {
+			
+		},
+		'Lost package': {
+			
+		},
+		'Discount not applied': {
+			
+		},
+		'Check order': {
+			
+		}*/
+	}
 }
