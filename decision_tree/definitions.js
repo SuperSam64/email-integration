32723be02eafd7eaa7d9a45af48b792865038a17
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-03-03 07:32:24 -0500

    Update definitions.js

--- decision_tree/definitions.js
+++ decision_tree/definitions.js
@@ -7,31 +7,50 @@ var data = {
 			'Eligible': {
 				title: 'Item(s) to return are eligible',
 				info: '',
-				'xCannot cancel': {
-					title: 'Cannot cancel',
-					info: 'Order could not be cancelled',
-					'xEmail to advise': {
-						title: 'Email to advise',
-						info: 'Email customer to advise cancellation was not possible',
+				'Return created': {
+					title: 'A return has not yet been created',
+					info: '',
+					'Create return': {
+						title: 'Create a return',
+						info: 'Create a return'
 					}
 				},
-				'xCan be cancelled': {
-					title: 'Can be cancelled',
-					info: 'Order can be cancelled',
-					'xEmail to advise': {
-						title: 'Email to advise',
-						info: 'Email customer to advise cancellation order was cancelled',
-					}
-				},
-				'xCan be partially cancelled': {
-					title: 'Can be partially cancelled',
-					info: 'Order could not be fully cancelled, but could be partially cancelled',
-					'xEmail to advise': {
-						title: 'Email to advise',
-						info: 'Email customer to advise order was partially cancelled',
+				'Return not created': {
+					title: 'A return has already been created',
+					info: '',
+					'Resend label(s)': {
+						title: 'Resend label(s)',
+						info: 'The customer needs a copy of the return label(s) resent',
+						'Resend labels': {
+							title: 'Resend label(s)',
+							info: 'Use the “View Return Label” link. Note that the link can be copied and pasted to be opened outside of your remote browser – it does not require you to use the VPN, or to be logged into a specific customer account. Opening it and saving it outside of the remote browser will make the file easier to find. Once you have opened the link, on the label page, press Ctrl + P to open the Print dialog. Choose “Save as PDF”, using the following options: Orientation: Portrait, Zoom: 200%, Margins: none [IMAGES] '
+						}
+					},
+					'Return additional item(s)': {
+						title: 'Return additional item(s)',
+						info: 'A partial return was created, but the customer needs to also return one or more item that was not included in the original return',
+						'Return additional': {
+							title: 'Send additional label(s)',
+							info: 'Send as many additional labels as needed using the “Generate Return Label [LINK]” link. When you create labels, choose the “Save as PDF” option. Make a [Monday request] to let CRMs know about the additional item(s) being returned, and the additional label(s). Include the tracking numbers for each additional label. '
+						}
+					},
+					'Overshipped': {
+						title: 'Overshipped',
+						info: 'Customer received a duplicate order/more items than they ordered',
+						'Send labels': {
+							title: 'Send label(s)',
+							info: 'Send as many labels as needed using the “Generate Return Label [LINK]” link. When you create labels, choose the “Save as PDF” option. Make a [Monday request] to let CRMs know about the item(s) being returned, and that no refund is owed. Include the tracking numbers for each additional label.'
+						}
+					},
+					'Create additional label(s)': {
+						title: 'Create additional label(s)',
+						info: 'A return was created for the correct item(s), but the customer needs additional label(s)',
+						'Send more labels': {
+							title: 'Send additional label(s)',
+							info: 'Send as many additional labels as needed using the “Generate Return Label [LINK]” link. When you create labels, choose the “Save as PDF” option. Make a [Monday request] to let CRMs know about the additional item(s) being returned, and the additional label(s). Include the tracking numbers for each additional label. '
+						}
 					}
 				}
-				
 			},
 			'Custom': {
 				title: 'Custom Air Filters',
