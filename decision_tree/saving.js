0e708ea56522a486a2e2a1189eb5111690142d40
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-03-30 11:13:35 -0400

    Update saving.js

--- decision_tree/saving.js
+++ decision_tree/saving.js
@@ -1,5 +1,20 @@
+var readButton = document.querySelector('#readButton');
+var saveButton = document.querySelector('#saveButton');
+var backupButton = document.querySelector('#backupButton');
+var restoreButton = document.querySelector('#restoreButton');
 
-read();
+readButton.addEventListener('click', function() {
+  read();
+});
+saveButton.addEventListener('click', function() {
+  save();
+});
+backupButton.addEventListener('click', function() {
+  backup();
+});
+restoreButton.addEventListener('click', function() {
+  restore();
+});
 
 function save(){
 
