681a40e94a6bdea9a133a62c2747813cd9a9ecbf
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-04-01 06:52:51 -0400

    Update saving.js

--- decision_tree/saving.js
+++ decision_tree/saving.js
@@ -1,3 +1,38 @@
+import { openDB } from 'https://unpkg.com';
+
+const DB_NAME = 'UserConfigDB';
+const STORE_NAME = 'configStore';
+
+async function getDB() {
+  return openDB(DB_NAME, 1, {
+    upgrade(db) {
+      db.createObjectStore(STORE_NAME);
+    },
+  });
+}
+
+export async function getConfig(key) {
+  const db = await getDB();
+  return db.get(STORE_NAME, key);
+}
+
+export async function setConfig(key, val) {
+  const db = await getDB();
+  return db.put(STORE_NAME, val, key);
+}
+
+export async function getAllConfig() {
+    const db = await getDB();
+    const allKeys = await db.getAllKeys(STORE_NAME);
+    const config = {};
+    for (const key of allKeys) {
+        config[key] = await db.get(STORE_NAME, key);
+    }
+    return config;
+}
+
+
+
 var readButton = document.querySelector('#readButton');
 var saveButton = document.querySelector('#saveButton');
 var backupButton = document.querySelector('#backupButton');
