ee447921750b0015f8bec56adf1b9731c52b6c7b
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2026-04-05 17:49:29 -0400

    Update saving.js

--- decision_tree/saving.js
+++ decision_tree/saving.js
@@ -1,23 +1,15 @@
-/* when nothing is in indexedDB, store a value in indexedDB that no data has been loaded. Offer to load config. Also offer not to ask again. If user opts out, do not show again. */
-
-
+/* when nothing is in indexedDB, store a value in indexedDB that no data has been loaded. Offer to load config. Also offer not to ask again. If user opts out, do not show again.
+check config data once a day
+*/
 var configData;
-
 const dbName = "ConfigDB";
 const storeName = "user_settings";
-const keyName = "current_config"
-
-
-var readButton = document.querySelector('#readButton');
 var saveButton = document.querySelector('#saveButton');
 var backupButton = document.querySelector('#backupButton');
 var restoreButton = document.querySelector('#restoreButton');
-
-readButton.addEventListener('click', function() {
-  loadFromDB("ConfigDB", "current_config", "user_settings");
-});
+setValues();
 saveButton.addEventListener('click', function() {
-  saveToDB("ConfigDB", getData(), "current_config", "user_settings");
+  saveToDB(getData());
 });
 backupButton.addEventListener('click', function() {
   exportConfig();
@@ -25,7 +17,6 @@ backupButton.addEventListener('click', function() {
 restoreButton.addEventListener('click', function() {
   importConfig();
 });
-
 function getData(){
   return JSON.stringify({
     first: document.querySelector('#first').value,
@@ -35,11 +26,8 @@ function getData(){
     }
   });
 }
-
-
-setValues();
 async function setValues(){
-  var imported = await loadFromDB("ConfigDB", "current_config", "user_settings");
+  var imported = await loadFromDB()
   if(imported){
     var importedJSON = JSON.parse(imported);
     var firstVal = document.querySelector('#first');
@@ -50,14 +38,17 @@ async function setValues(){
     thirdVal.value = importedJSON.second.secondB;
     configData = imported;
     console.log('success');
+    document.querySelector('#messageSection').innerText = 'Stored settings are in use';
   }
   else{
-    console.log('failure');
+    noSaveData();
+    document.querySelector('#messageSection').innerHTML = '<span>Settings not found. This can occur after clearing cookies. Restore from backup?<span>&nbsp;&nbsp;<span style="color:blue;text-decoration:underline" onclick="javascript:(document.querySelector(`#restoreButton`).click())">Restore</span>';
   }
 }
-
-
-async function initDB(dbName, storeName) {
+function noSaveData(){
+  document.querySelector('#messageSection').innerHTML = '<span>Your settings have not been backed up. Would you like to back them up now?<span>&nbsp;&nbsp;<span style="color:blue;text-decoration:underline" onclick="javascript:(document.querySelector(`#backupButton`).click())">Back up</span>';;
+}
+async function initDB() {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(dbName, 1);
     request.onupgradeneeded = (e) => e.target.result.createObjectStore(storeName);
@@ -65,41 +56,12 @@ async function initDB(dbName, storeName) {
     request.onerror = () => reject(request.error);
   });
 }
-
-async function saveToDB(dbName, savedValue, keyName, storeName) {
-  console.log('saving...');
-  const db = await initDB(dbName, storeName);
-  const tx = db.transaction(storeName, "readwrite");
-  tx.objectStore(storeName).put(savedValue, keyName);
-}
-
-async function loadFromDB(dbName, keyName, storeName) {
-  console.log('loading...');
-  const db = await initDB(dbName,, storeName);
-  return new Promise((resolve) => {
-    const req = db.transaction(storeName).objectStore(storeName).get(keyName);
-    req.onsuccess = () => resolve(req.result);
-  });
-}
-
-
-
-/*async function initDB() {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open(dbName, 1);
-    request.onupgradeneeded = (e) => e.target.result.createObjectStore(storeName);
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject(request.error);
-  });
-}
-
-async function (config) {
+async function saveToDB(config) {
   console.log('saving...');
   const db = await initDB();
   const tx = db.transaction(storeName, "readwrite");
   tx.objectStore(storeName).put(config, "current_config");
 }
-
 async function loadFromDB() {
   console.log('loading...');
   const db = await initDB();
@@ -107,10 +69,9 @@ async function loadFromDB() {
     const req = db.transaction(storeName).objectStore(storeName).get("current_config");
     req.onsuccess = () => resolve(req.result);
   });
-}*/
-
-
+}
 async function exportConfig() {
+  await saveToDB(getData());
   console.log('export initiated');
   const config = await loadFromDB();
   const handle = await window.showSaveFilePicker({
@@ -118,20 +79,19 @@ async function exportConfig() {
     types: [{ description: 'JSON', accept: {'application/json': ['.json']} }]
   });
   const writable = await handle.createWritable();
-  await writable.write(JSON.stringify(config, null, 2));
+  await writable.write(JSON.stringify(config));
   await writable.close();
 }
-
 async function importConfig() {
   const [handle] = await window.showOpenFilePicker();
   const file = await handle.getFile();
   const config = JSON.parse(await file.text());
-  await saveToDB("ConfigDB", config, "current_config", "user_settings"); // Sync to DB after import
+  await saveToDB(config); // Sync to DB after import
+  setValues();
   return config;
 }
-
 /*
-1) No safe file found. This can occur after browsing data has been cleared. Please restore your settings from a backup, or configure your settings again. Note: the default name and location of your backups will be Documents/decision_tree/config.json
+1) No save file found. This can occur after browsing data has been cleared. Please restore your settings from a backup, or configure your settings again. Note: the default name and location of your backups will be Documents/decision_tree/config.json
 2) Configuration complete. Would you like to backup this configuration?
 3) Configuration restored from backup.*/
 
